fd39385743364b2f33cc9cd58c3006c8b8203f05
Pablo <pablogp01@gmail.com>
2024-11-23 23:02:59 +0000

    Teams/Drivers Screen first commit

---
+++ app/src/main/java/com/example/jedapaappf1/TeamsDriversScreen.kt
@@ -0,0 +1,44 @@
+package com.example.jedapaappf1
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+
+@Composable
+
+fun TeamsDriversScreen(navController: NavHostController){
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize().background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+            /////////////////////////////HEADER/////////////////////////////////
+            MyHeader(modifier = Modifier, { navController.navigate("signup") }, { navController.navigate("login") })
+            ////////////////////////////////////////////////////////////////////
+
+            Text("Teams/Drivers Page")
+
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TeamsDriversScreenPreview() {
+    TeamsDriversScreen(navController = NavHostController(LocalContext.current))
+}
