0ef5aab4d6d4dec3527101e7b0265e865313a920
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2024-11-25 01:51:48 +0000

    Posted data for each circuit on the calendar

--- app/src/main/java/com/example/jedapaappf1/CalendarScreen.kt
+++ app/src/main/java/com/example/jedapaappf1/CalendarScreen.kt
@@ -30,25 +30,101 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 
-data class Circuito(val nombre: String, val imagenId: Int)
+data class Circuito(
+    val nombre: String,
+    val imagenId: Int,
+    val datosFechasHoras: List<Triple<String, String, String>>
+)
 
 @Composable
 fun CalendarScreen(navController: NavHostController, userViewModel: UserViewModel = viewModel()) {
     val formula1Font = FontFamily(Font(R.font.formula1_bold))
 
     val circuitos = listOf(
-        Circuito("Circuito de Bahrain", R.drawable.bahrain_circuit),
-        Circuito("Circuito de Catalunya", R.drawable.catalunya_circuit),
-        Circuito("Circuito de Hungaroring", R.drawable.hungaroring_circuit),
-        Circuito("Circuito de Imola", R.drawable.imola_circuit),
-        Circuito("Circuito de Mónaco", R.drawable.monaco_circuit),
-        Circuito("Circuito de Monza", R.drawable.monza_circuit),
-        Circuito("Circuito de Mugello", R.drawable.mugello_circuit),
-        Circuito("Circuito de Portimao", R.drawable.portimao_circuit),
-        Circuito("Circuito de Red Bull Ring", R.drawable.red_bull_ring_circuit),
-        Circuito("Circuito de Silverstone", R.drawable.silverstone_circuit),
-        Circuito("Circuito de Sochi", R.drawable.sochi_circuit),
-        Circuito("Circuito de Spa-Francorchamps", R.drawable.spa_francorchamps_circuit),
+        Circuito("Circuito de Bahrain", R.drawable.bahrain_circuit, listOf(
+            Triple("race", "02/03/2024", "15:00"),
+            Triple("qualifying", "01/03/2024", "14:00"),
+            Triple("practice3", "01/03/2024", "11:00"),
+            Triple("practice2", "28/02/2024", "15:30"),
+            Triple("practice1", "28/02/2024", "11:00")
+        )),
+        Circuito("Circuito de Catalunya", R.drawable.catalunya_circuit, listOf(
+            Triple("race", "23/06/2024", "14:00"),
+            Triple("qualifying", "22/06/2024", "15:00"),
+            Triple("practice3", "22/06/2024", "11:30"),
+            Triple("practice2", "21/06/2024", "16:00"),
+            Triple("practice1", "21/06/2024", "12:30")
+        )),
+        Circuito("Circuito de Hungaroring", R.drawable.hungaroring_circuit, listOf(
+            Triple("race", "21/07/2024", "14:00"),
+            Triple("qualifying", "20/07/2024", "15:00"),
+            Triple("practice3", "20/07/2024", "11:30"),
+            Triple("practice2", "19/07/2024", "16:00"),
+            Triple("practice1", "19/07/2024", "12:30")
+        )),
+        Circuito("Circuito de Imola", R.drawable.imola_circuit, listOf(
+            Triple("race", "19/05/2024", "14:00"),
+            Triple("qualifying", "18/05/2024", "15:00"),
+            Triple("practice3", "18/05/2024", "11:30"),
+            Triple("practice2", "17/05/2024", "16:00"),
+            Triple("practice1", "17/05/2024", "12:30")
+        )),
+        Circuito("Circuito de Mónaco", R.drawable.monaco_circuit, listOf(
+            Triple("race", "26/05/2024", "14:00"),
+            Triple("qualifying", "25/05/2024", "15:00"),
+            Triple("practice3", "25/05/2024", "11:30"),
+            Triple("practice2", "24/05/2024", "16:00"),
+            Triple("practice1", "24/05/2024", "12:30")
+        )),
+        Circuito("Circuito de Monza", R.drawable.monza_circuit, listOf(
+            Triple("race", "01/09/2024", "14:00"),
+            Triple("qualifying", "31/08/2024", "15:00"),
+            Triple("practice3", "31/08/2024", "11:30"),
+            Triple("practice2", "30/08/2024", "16:00"),
+            Triple("practice1", "30/08/2024", "12:30")
+        )),
+        Circuito("Circuito de Mugello", R.drawable.mugello_circuit, listOf(
+            Triple("race", "13/09/2024", "15:10"),
+            Triple("qualifying", "12/09/2024", "15:00"),
+            Triple("practice3", "12/09/2024", "12:00"),
+            Triple("practice2", "11/09/2024", "15:00"),
+            Triple("practice1", "11/09/2024", "11:00")
+        )),
+        Circuito("Circuito de Portimao", R.drawable.portimao_circuit, listOf(
+            Triple("race", "02/05/2024", "16:00"),
+            Triple("qualifying", "01/05/2024", "16:00"),
+            Triple("practice3", "01/05/2024", "13:00"),
+            Triple("practice2", "30/04/2024", "16:00"),
+            Triple("practice1", "30/04/2024", "12:30")
+        )),
+        Circuito("Circuito de Red Bull Ring", R.drawable.red_bull_ring_circuit, listOf(
+            Triple("race", "30/06/2024", "14:00"),
+            Triple("qualifying", "29/06/2024", "15:00"),
+            Triple("practice3", "29/06/2024", "11:00"),
+            Triple("practice2", "28/06/2024", "15:30"),
+            Triple("practice1", "28/06/2024", "11:30")
+        )),
+        Circuito("Circuito de Silverstone", R.drawable.silverstone_circuit, listOf(
+            Triple("race", "07/07/2024", "15:00"),
+            Triple("qualifying", "06/07/2024", "15:00"),
+            Triple("practice3", "06/07/2024", "11:30"),
+            Triple("practice2", "05/07/2024", "16:00"),
+            Triple("practice1", "05/07/2024", "12:30")
+        )),
+        Circuito("Circuito de Sochi", R.drawable.sochi_circuit, listOf(
+            Triple("race", "26/09/2024", "14:00"),
+            Triple("qualifying", "25/09/2024", "14:00"),
+            Triple("practice3", "25/09/2024", "11:00"),
+            Triple("practice2", "24/09/2024", "14:00"),
+            Triple("practice1", "24/09/2024", "10:30")
+        )),
+        Circuito("Circuito de Spa-Francorchamps", R.drawable.spa_francorchamps_circuit, listOf(
+            Triple("race", "28/07/2024", "14:00"),
+            Triple("qualifying", "27/07/2024", "15:00"),
+            Triple("practice3", "27/07/2024", "11:30"),
+            Triple("practice2", "26/07/2024", "16:00"),
+            Triple("practice1", "26/07/2024", "12:30")
+        )),
     )
 
     Box(
@@ -86,16 +162,17 @@ fun CalendarScreen(navController: NavHostController, userViewModel: UserViewMode
 
 @Composable
 fun CircuitoItem(circuito: Circuito) {
+    val formula1Font = FontFamily(Font(R.font.formula1_bold))
     var isFlipped by remember { mutableStateOf(false) }
 
+    // Animación de rotación
     val rotation by animateFloatAsState(targetValue = if (isFlipped) 180f else 0f)
     val frontAlpha = if (rotation <= 90f) 1f else 0f
     val backAlpha = if (rotation > 90f) 1f else 0f
     val textRotation = if (isFlipped) 180f else 0f
 
     Box(
-        modifier = Modifier
-            .padding(8.dp).width(300.dp).height(200.dp)
+        modifier = Modifier.padding(8.dp).width(300.dp).height(200.dp)
             .clickable { isFlipped = !isFlipped }
             .graphicsLayer {
                 rotationY = rotation
@@ -103,6 +180,7 @@ fun CircuitoItem(circuito: Circuito) {
             },
         contentAlignment = Alignment.Center
     ) {
+        // Lado frontal
         if (frontAlpha > 0f) {
             Image(
                 painter = painterResource(id = circuito.imagenId),
@@ -112,18 +190,68 @@ fun CircuitoItem(circuito: Circuito) {
             )
         }
 
+        // Lado posterior
         if (backAlpha > 0f) {
-            Text(
-                text = circuito.nombre, fontSize = 20.sp, color = Color.Black,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxSize()
-                    .graphicsLayer { rotationY = textRotation }
-                    .alpha(backAlpha)
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize().graphicsLayer { rotationY = textRotation }
+                    .alpha(backAlpha).padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Título del circuito
+                Text(
+                    text = circuito.nombre,
+                    fontSize = 18.sp, fontFamily = formula1Font,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
+
+                // Lista de eventos, fechas y horas
+                circuito.datosFechasHoras.forEach { (evento, fecha, hora) ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        // Columna para el evento
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(
+                                text = evento, fontSize = 14.sp,
+                                color = Color.Black, textAlign = TextAlign.Center
+                            )
+                        }
+
+                        // Columna para la fecha
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(
+                                text = "$fecha", fontSize = 14.sp,
+                                color = Color.Black, textAlign = TextAlign.Center
+                            )
+                        }
+
+                        // Columna para la hora
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            Text(
+                                text = "$hora", fontSize = 14.sp,
+                                color = Color.Black, textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
